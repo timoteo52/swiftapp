dba4dd954eb7228117c6cd1edded63b5e9b53b8d
poppydoodle <poppy@poppydoodles-macbook-pro.local>
2016-12-29 22:33:10 -0600

    Initial Commit

---
+++ Swift App 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App 1
+//
+//  Created by poppydoodle on 12/29/16.
+//  Copyright © 2016 poppydoodle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
